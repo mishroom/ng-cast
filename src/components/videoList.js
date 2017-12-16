a47dafa428b6c10a8afa53b1ea44131884ef69a5
mishroom <mishall.afzal@gmail.com>
2017-12-15 16:13:00 -0800

    all components correctly rendering + data passed into each video

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,9 @@
 angular.module('video-player')
-.component('videoList', {
-  // TODO
-});
+  .component('videoList', {
+    // TODO
+    templateUrl: 'src/templates/videoList.html',
+    controller: function () {
+      this.data = window.exampleVideoData;
+      
+    }
+  });
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,11 @@
 angular.module('video-player')
-.component('videoListEntry', {
-  // TODO
-});
+  .component('videoListEntry', {
+    controller: function() {
+      // this.video = '';
+      console.log(this);
+    },
+    bindings: {
+      video: '<'
+    },
+    templateUrl: 'src/templates/videoListEntry.html'
+  });
